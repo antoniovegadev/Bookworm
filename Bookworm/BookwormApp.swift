3d418cdc01f6bedb11fb8e73afda20608e97c16c
antonivegaochoa <tonivegaochoa@gmail.com>
2021-05-16 12:15:04 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Antonio Vega on 5/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
